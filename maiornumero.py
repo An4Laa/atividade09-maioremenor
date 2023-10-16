4a92c60a471c63bb6acbbb038e3ac565586d4b29
An4Laa <analaurarochadesouza6@gmail.com>
2023-10-16 15:16:36 -0300

    Atividade maior e menor

---
+++ maiornumero.py
@@ -0,0 +1,6 @@
+numero1 = int(input("Digite um número inteiro: "))
+numero2 = int(input("Digite outro número inteiro: "))
+if numero1 > numero2 :
+    print(f"O número {numero1} é o maior número.")
+else :
+    print(f"O número {numero2} é o maior número.")
